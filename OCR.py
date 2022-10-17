18c712a42693839f70bd98d46bb9d4ad831b52ee
Batuhan Tomo <batuhan.tomo@gmail.com>
2022-10-18 00:58:56 +0300

    Update

--- OCR.py
+++ OCR.py
@@ -7,18 +7,41 @@ import os
 import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import ttk
+from tkinter import *
 from os.path import exists as file_exists
+import sv_ttk
+
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\Tesseract.exe'
 
 options = {"English":"en","Turkish":"tr","German":"de","French":"fr","Italian":"it","Spanish ":"es","Russian":"ru","Portuguese(Brazil)":"br","Danish":"da","Dutch":"nl","Polish":"pl","Ukrainian":"uk","Afrikaans":"af","Arabic":"ar","Bengali":"bn","Bulgarian":"bg","Catalan":"ca","Chinese":"yue","Czech":"cs","Filipino":"fil","Finnish":"fi","Greek":"el","Gujarati":"gu","Hindi":"hi","Hungarian":"hu","Icelandic":"is","Indonesian":"id","Japanese":"ja","Kannada":"kn","Korean":"ko","Latvian":"lv","Malay":"ms","Malayalam":"ml","Mandarin Chinese":"cmn","Norwegian":"nb","Portuguese(Portugal)":"pt","Punjabi":"pa","Romanian":"ro","Serbian":"sr","Slovak":"sk","Swedish":"sv","Tamil":"ta","Telugu":"te","Thai":"th","Vietnamese":"vi"} 
 
 root= tk.Tk()
-root.geometry("450x230")
+root.geometry("600x350")
 root.title("OCR & Text To Speech - © 2022 Batuhan Tomo")
 root.resizable(False, False)
-style = ttk.Style(root)
-style.theme_use()
+sv_ttk.set_theme("dark")
+
+canvas = tk.Canvas(root, width = 500, height = 400)
+canvas.pack()
+
+is_on = True
+def switch():
+    global is_on
+    
+    if is_on:
+        on_button.config(image = off)
+        sv_ttk.set_theme("light")
+        is_on = False
+    else:
+        on_button.config(image = on)
+        sv_ttk.set_theme("dark")
+        is_on = True
+    
+on = PhotoImage(file = "img/on.png")
+off = PhotoImage(file = "img/off.png")
+on_button = Button(root, image = on, bd = 0, command = switch)
+canvas.create_window(20, 320, window=on_button)
 
 def show():
     global lang
@@ -27,27 +50,27 @@ def show():
 
 material = tk.StringVar()
 
-canvas = tk.Canvas(root, width = 500, height = 400)
-canvas.pack()
-
 combo = ttk.Combobox(root, state = "readonly", values= list(options.keys()), textvariable=material)
 combo.current()
 
 combo.bind("<<ComboboxSelected>>", lambda event:[show(),entry2.delete(0,"end"),(entry2.insert(0,options[material.get()]))])
-canvas.create_window(230, 150, window=combo)
+canvas.create_window(250, 215, window=combo)
 
-label = tk.Label(text="Lütfen Seslendirilmesini İstediğiniz Dosyanın Yolunu Giriniz. \n Elle Girmek İstemezseniz Dosya Seç Tuşuna Basarak Dosyayı Seçebilirsiniz.")
-canvas.create_window(230, 40, window=label)
+label = tk.Label(text="Lütfen Seslendirilmesini İstediğiniz Dosyanın Yolunu Giriniz. \n Elle Girmek İstemezseniz Dosya Seç Tuşuna Basarak Dosyayı Seçebilirsiniz.",font='Helvetica 10 bold')
+canvas.create_window(250, 60, window=label)
 
-label2 = tk.Label(text="Lütfen seslendirme dilini seçiniz.")
-canvas.create_window(230, 120, window=label2)
+label2 = tk.Label(text="Lütfen seslendirme dilini seçiniz.", font='Helvetica 10 bold')
+canvas.create_window(250, 180, window=label2)
+
+label3 = tk.Label(text="Temayı Değiştir", font='Helvetica 10 bold')
+canvas.create_window(20, 280,window=label3)
 
 def setTextInput(text):
     entry1.delete(0,"end")
     entry1.insert(0, text)
     
 entry1 = tk.Entry()
-canvas.create_window(200, 80, width= 300, window=entry1)
+canvas.create_window(220, 120, width= 420, window=entry1)
 
 entry2 = tk.Entry()
 
@@ -56,8 +79,8 @@ def fileSelect():
     global fz
     fz = fd.askopenfilename(title='Open files', initialdir='/', filetypes=filetypes)
 
-open_button = ttk.Button(root,text='Dosya Seç',command=lambda:[fileSelect(),setTextInput(fz)])
-canvas.create_window(390, 80, window=open_button)
+open_button = ttk.Button(root,text='Dosya Seç', style='Accent.TButton', command=lambda:[fileSelect(),setTextInput(fz)])
+canvas.create_window(480, 120, window=open_button)
 
     
 def Process():
@@ -111,7 +134,7 @@ def Process():
     # cv2.imshow("image 1", img)
     # cv2.waitKey(0)
  
-button1 = tk.Button(text='Oku', command=lambda:[Process()])
-canvas.create_window(230, 190, height= 35 ,width=90 , window=button1)
+button1 = ttk.Button(text='Seslendir', style='Accent.TButton',command=lambda:[Process()])
+canvas.create_window(250, 285, height= 35 ,width=90 , window=button1)
 
 root.mainloop()
